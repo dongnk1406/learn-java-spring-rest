d4337f755914ce1c438ff2eae36502a706342f07
nkdong <khac.nguyen@brighte.com.au>
2025-05-23 15:46:01 +0700

    feat: init project and sample CRUD user

---
+++ src/main/java/com/stephen/spring_boot_api/service/UserService.java
@@ -0,0 +1,47 @@
+package com.stephen.spring_boot_api.service;
+
+import com.stephen.spring_boot_api.dto.request.UserCreationRequest;
+import com.stephen.spring_boot_api.dto.request.UserUpdateRequest;
+import com.stephen.spring_boot_api.entity.User;
+import com.stephen.spring_boot_api.repository.UserRepository;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+    @Autowired
+    private UserRepository userRepository;
+
+    public User createUser(UserCreationRequest request) {
+        User user = new User();
+        user.setUsername(request.getUsername());
+        user.setPassword(request.getPassword());
+        user.setFirstName(request.getFirstName());
+        user.setLastName(request.getLastName());
+        user.setDateOfBirth(request.getDateOfBirth());
+        return userRepository.save(user);
+    }
+
+    public List<User> getUsers() {
+        return userRepository.findAll();
+    }
+
+    public User getUser(String userId) {
+        return userRepository.findById(userId).orElseThrow(() -> new RuntimeException("User not found"));
+    }
+
+    public User updateUser(String userId, UserUpdateRequest request) {
+        User user = getUser(userId);
+        user.setPassword(request.getPassword());
+        user.setFirstName(request.getFirstName());
+        user.setLastName(request.getLastName());
+        user.setDateOfBirth(request.getDateOfBirth());
+        return userRepository.save(user);
+    }
+    public void deleteUser(String userId) {
+        userRepository.deleteById(userId);
+    }
+}
